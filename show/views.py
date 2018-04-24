b16469e16b4d1ec0c530bf628939be0ac30639ae
WanHe <1556820213@qq.com>
2018-04-24 13:52:07 +0800

    unfinish the sequence questions

--- show/views.py
+++ show/views.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
 from django.shortcuts import render
 from upload.models import Ques
 # Create your views here.
 
+index = 0
+
 def show(request):
+	index = 0
 	all_ques = Ques.objects.all()
 	questions = list(all_ques)
-	question = questions[0]
-	return render(request,'show/showImg.html',{"question":question})
+	number = len(questions)
+	question = questions[index]
+	return render(request,'show/showImg.html',{"Question":question})
+
 
 def index(request):
 	return render(request,'show/index.html')
+
+def get_next(request):
+	index += 1
+	return HttpResponse("Success")
+
+
+
